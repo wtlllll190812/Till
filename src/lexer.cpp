ba08b2c64e68141c6ff7620c52942044628e61c6
wtlllll <3200102777@zju.edu.cn>
2023-04-30 19:17:39 +0800

    [add]祛除ast对parser的依赖

--- src/lexer.cpp
+++ src/lexer.cpp
@@ -366,10 +366,10 @@ struct yy_trans_info
 	};
 static const flex_int16_t yy_accept[65] =
     {   0,
-        0,    0,   31,   30,    1,   30,   30,    3,    4,   18,
-       16,    8,   17,   19,   27,    7,   12,    9,   13,   26,
-       26,   26,   26,   26,   26,   26,    5,    6,   11,    0,
-       29,    0,    0,   28,   27,   14,   10,   15,   26,   26,
+        0,    0,   31,   30,    1,   30,   30,   13,   14,   11,
+        9,   18,   10,   12,   27,   17,    5,   19,    6,   26,
+       26,   26,   26,   26,   26,   26,   15,   16,    4,    0,
+       29,    0,    0,   28,   27,    7,    3,    8,   26,   26,
        26,   22,   26,   26,   26,    0,    0,    0,    0,   28,
        26,   26,   21,   26,   26,    2,   23,   20,   26,   26,
        26,   24,   25,    0
@@ -494,8 +494,9 @@ char *yytext;
 #include "parser.hpp"
 #define SAVE_TOKEN yylval.string = new std::string(yytext, yyleng)
 #define TOKEN(t) (yylval.token = t)
-#line 497 "src/lexer.cpp"
+#define OPERA(op) (yylval.token = op)
 #line 498 "src/lexer.cpp"
+#line 499 "src/lexer.cpp"
 
 #define INITIAL 0
 
@@ -712,9 +713,9 @@ YY_DECL
 		}
 
 	{
-#line 10 "lang/till.l"
+#line 11 "lang/till.l"
 
-#line 717 "src/lexer.cpp"
+#line 718 "src/lexer.cpp"
 
 	while ( /*CONSTCOND*/1 )		/* loops until end-of-file is reached */
 		{
@@ -774,143 +775,143 @@ do_action:	/* This label is used only to access EOF actions. */
 case 1:
 /* rule 1 can match eol */
 YY_RULE_SETUP
-#line 11 "lang/till.l"
+#line 12 "lang/till.l"
 ;/* ignore whitespace */
 	YY_BREAK
 case 2:
 /* rule 2 can match eol */
 YY_RULE_SETUP
-#line 12 "lang/till.l"
+#line 13 "lang/till.l"
 ;/* ignore comments */
 	YY_BREAK
 case 3:
 YY_RULE_SETUP
-#line 13 "lang/till.l"
-TOKEN(LPAREN); return LPAREN;
+#line 14 "lang/till.l"
+TOKEN((int)Opera::EQ); return EQ;
 	YY_BREAK
 case 4:
 YY_RULE_SETUP
-#line 14 "lang/till.l"
-TOKEN(RPAREN); return RPAREN;
+#line 15 "lang/till.l"
+TOKEN((int)Opera::NE); return NE;
 	YY_BREAK
 case 5:
 YY_RULE_SETUP
-#line 15 "lang/till.l"
-TOKEN(LBRACE); return LBRACE;
+#line 16 "lang/till.l"
+TOKEN((int)Opera::LT); return LT;
 	YY_BREAK
 case 6:
 YY_RULE_SETUP
-#line 16 "lang/till.l"
-TOKEN(RBRACE); return RBRACE;
+#line 17 "lang/till.l"
+TOKEN((int)Opera::GT); return GT;
 	YY_BREAK
 case 7:
 YY_RULE_SETUP
-#line 17 "lang/till.l"
-TOKEN(SEMICOLON); return SEMICOLON;
+#line 18 "lang/till.l"
+TOKEN((int)Opera::LE); return LE;
 	YY_BREAK
 case 8:
 YY_RULE_SETUP
-#line 18 "lang/till.l"
-TOKEN(COMMA); return COMMA;
+#line 19 "lang/till.l"
+TOKEN((int)Opera::GE); return GE;
 	YY_BREAK
 case 9:
 YY_RULE_SETUP
-#line 19 "lang/till.l"
-TOKEN(ASSIGN); return ASSIGN;
+#line 20 "lang/till.l"
+TOKEN((int)Opera::ADD); return ADD;
 	YY_BREAK
 case 10:
 YY_RULE_SETUP
-#line 20 "lang/till.l"
-TOKEN(EQ); return EQ;
+#line 21 "lang/till.l"
+TOKEN((int)Opera::SUB); return SUB;
 	YY_BREAK
 case 11:
 YY_RULE_SETUP
-#line 21 "lang/till.l"
-TOKEN(NE); return NE;
+#line 22 "lang/till.l"
+TOKEN((int)Opera::MUL); return MUL;
 	YY_BREAK
 case 12:
 YY_RULE_SETUP
-#line 22 "lang/till.l"
-TOKEN(LT); return LT;
+#line 23 "lang/till.l"
+TOKEN((int)Opera::DIV); return DIV;
 	YY_BREAK
 case 13:
 YY_RULE_SETUP
-#line 23 "lang/till.l"
-TOKEN(GT); return GT;
+#line 24 "lang/till.l"
+TOKEN(LPAREN); return LPAREN;
 	YY_BREAK
 case 14:
 YY_RULE_SETUP
-#line 24 "lang/till.l"
-TOKEN(LE); return LE;
+#line 25 "lang/till.l"
+TOKEN(RPAREN); return RPAREN;
 	YY_BREAK
 case 15:
 YY_RULE_SETUP
-#line 25 "lang/till.l"
-TOKEN(GE); return GE;
+#line 26 "lang/till.l"
+TOKEN(LBRACE); return LBRACE;
 	YY_BREAK
 case 16:
 YY_RULE_SETUP
-#line 26 "lang/till.l"
-TOKEN(ADD); return ADD;
+#line 27 "lang/till.l"
+TOKEN(RBRACE); return RBRACE;
 	YY_BREAK
 case 17:
 YY_RULE_SETUP
-#line 27 "lang/till.l"
-TOKEN(SUB); return SUB;
+#line 28 "lang/till.l"
+TOKEN(SEMICOLON); return SEMICOLON;
 	YY_BREAK
 case 18:
 YY_RULE_SETUP
-#line 28 "lang/till.l"
-TOKEN(MUL); return MUL;
+#line 29 "lang/till.l"
+TOKEN(COMMA); return COMMA;
 	YY_BREAK
 case 19:
 YY_RULE_SETUP
-#line 29 "lang/till.l"
-TOKEN(DIV); return DIV;
+#line 30 "lang/till.l"
+TOKEN(ASSIGN); return ASSIGN;
 	YY_BREAK
 case 20:
 YY_RULE_SETUP
-#line 30 "lang/till.l"
+#line 31 "lang/till.l"
 TOKEN(FUNC); return FUNC;
 	YY_BREAK
 case 21:
 YY_RULE_SETUP
-#line 31 "lang/till.l"
+#line 32 "lang/till.l"
 TOKEN(LET); return LET;
 	YY_BREAK
 case 22:
 YY_RULE_SETUP
-#line 32 "lang/till.l"
+#line 33 "lang/till.l"
 TOKEN(IF); return IF;
 	YY_BREAK
 case 23:
 YY_RULE_SETUP
-#line 33 "lang/till.l"
+#line 34 "lang/till.l"
 TOKEN(ELSE); return ELSE;
 	YY_BREAK
 case 24:
 YY_RULE_SETUP
-#line 34 "lang/till.l"
+#line 35 "lang/till.l"
 TOKEN(WHILE); return WHILE;
 	YY_BREAK
 case 25:
 YY_RULE_SETUP
-#line 35 "lang/till.l"
+#line 36 "lang/till.l"
 TOKEN(RETURN); return RETURN;
 	YY_BREAK
 case 26:
 YY_RULE_SETUP
-#line 36 "lang/till.l"
+#line 37 "lang/till.l"
 SAVE_TOKEN; return IDENTIFIER;
 	YY_BREAK
 case 27:
 YY_RULE_SETUP
-#line 37 "lang/till.l"
+#line 38 "lang/till.l"
 SAVE_TOKEN; return INTEGER;
 	YY_BREAK
 case 28:
 YY_RULE_SETUP
-#line 38 "lang/till.l"
+#line 39 "lang/till.l"
 SAVE_TOKEN; return DOUBLE;
 	YY_BREAK
 case 29:
@@ -918,15 +919,15 @@ case 29:
 (yy_c_buf_p) = yy_cp -= 1;
 YY_DO_BEFORE_ACTION; /* set up yytext again */
 YY_RULE_SETUP
-#line 39 "lang/till.l"
+#line 40 "lang/till.l"
 SAVE_TOKEN; return STRING;
 	YY_BREAK
 case 30:
 YY_RULE_SETUP
-#line 40 "lang/till.l"
+#line 41 "lang/till.l"
 ECHO;
 	YY_BREAK
-#line 929 "src/lexer.cpp"
+#line 930 "src/lexer.cpp"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -1931,5 +1932,5 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 40 "lang/till.l"
+#line 41 "lang/till.l"
 
